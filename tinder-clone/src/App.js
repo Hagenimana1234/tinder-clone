5a241bdb8cac65973386bc6b08d7ea14cd89a541
hagenimana felicien <hagenimanafelicien9@gmail.com>
2021-03-25 17:14:36 +0200

    I have installed a create-react-app

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,14 @@
+
+import react from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build a tinder clone </h1>
+    </div>
+  );
+}
+
+export default App;
